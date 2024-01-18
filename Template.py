58e82b41fcca7153fe14ba2717c2e9940ea19ab7
Sempiternalll <100777000+Sempiternalll@users.noreply.github.com>
2024-01-19 02:36:01 +0330

    '&" swapped back

--- Template.py
+++ Template.py
@@ -64,7 +64,7 @@ class App(tkinter.Tk):
         self.marker_list = []  # Keeping track of markers
 
     def check_connections(self, results):
-        print('result2 ', results)
+        #print('result2 ', results)
         locations = []
         dfA = pd.read_csv('Adjacency_matrix.csv')
         prolog.retractall("directly_connected(_,_)")
@@ -74,8 +74,8 @@ class App(tkinter.Tk):
             locations.append(city)
             for index, row in dfA.iterrows():
                 if row[city] == 1:
-                    s = 'directly_connected("'
-                    s += city + '","' + row['Destinations'] + '")'
+                    s = "directly_connected('"
+                    s += city + "', '" + row['Destinations'] + "')"
                     print(s)
                     prolog.assertz(s)
             prolog.assertz("connected(X,Y) :- directly_connected(X,Z), directly_connected(Z,Y)")
@@ -89,12 +89,11 @@ class App(tkinter.Tk):
         """Extract locations from the text area and mark them on the map."""
         text = self.text_area.get("1.0", "end-1c")  # Get text from text area
         locations = self.extract_locations(text)  # Extract locations (you may use a more complex method here)
-        print(locations)
         # TODO 4: create the query based on the extracted features of user desciption 
         ################################################################################################
         query = locations
+        print('query = ',query)
         results = list(prolog.query(query))
-        print(results)
         locations = self.check_connections(results)
         # TODO 6: if the number of destinations is less than 6 mark and connect them 
         ################################################################################################
@@ -126,43 +125,56 @@ class App(tkinter.Tk):
             self.marker_path = self.map_widget.set_path(position_list)
 
     def extract_locations(self, text):
-        print(type(text))
         """Extract locations from text. A placeholder for more complex logic."""
         # Placeholder: Assuming each line in the text contains a single location name
         # TODO 3: extract key features from user's description of destinations
         ################################################################################################
-        s = 'destination(City,'
+        s = "destination(City, "
         dfD = pd.read_csv('Destinations.csv')
+        S = ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
         for index, row in dfD.iterrows():
-            co = re = cl = bu = ac = de = du = cu = hi = na = ac = la = '_'
-            if row['country'] in text:
-                co = row['country']
-            if row['region'] in text:
-                re = row['region']
-            if row['Climate'] in text:
-                cl = row['Climate']
-            if row['Budget'] in text:
-                bu = row['Budget']
-            if row['Activity'] in text:
-                ac = row['Activity']
-            if row['Duration'] in text:
-                du = row['Duration']
-            if row['Cuisine'] in text:
-                cu = row['Cuisine']
-            if row['History'] in text:
-                hi = row['History']
-            if row['Natural Wonder'] in text:
-                na = row['Natural Wonder']
-            if row['Accommodation'] in text:
-                ac = row['Accommodation']
-            if row['Language'] in text:
-                la = row['Language']
-            if row['Demographics'] in text:
-                de = row['Demographics']
-        s += co + ',' + re + ',' + cl + ',' + bu + ','
-        s += ac + ',' + de + ',' + du + ',' + cu + ','
-        s += hi + ',' + na + ',' + ac + ',' + la + ')'
-        print('extracted: ', s)
+            i = 0
+            for column in row:
+                if column in text:
+                    S[i] = column
+                i += 1
+            # co = re = cl = bu = ac = de = du = cu = hi = na = ac = la = '_'
+            # if row['country'] in text:
+            #     S[0] = row['country']
+            # if row['region'] in text:
+            #     S[0] = row['region']
+            # if row['Climate'] in text:
+            #     S[0] = row['Climate']
+            # if row['Budget'] in text:
+            #     S[0] = row['Budget']
+            # if row['Activity'] in text:
+            #     S[0] = row['Activity']
+            # if row['Duration'] in text:
+            #     S[0] = row['Duration']
+            # if row['Cuisine'] in text:
+            #     S[0] = row['Cuisine']
+            # if row['History'] in text:
+            #     S[0] = row['History']
+            # if row['Natural Wonder'] in text:
+            #     S[0] = row['Natural Wonder']
+            # if row['Accommodation'] in text:
+            #     S[0] = row['Accommodation']
+            # if row['Language'] in text:
+            #     la = row['Language']
+            # if row['Demographics'] in text:
+            #     de = row['Demographics']
+        # s += co + ',' + re + ',' + cl + ',' + bu + ','
+        # s += ac + ',' + de + ',' + du + ',' + cu + ','
+        # s += hi + ',' + na + ',' + ac + ',' + la + ')'
+        for i in range(1, 12):
+            if S[i] != '_':
+                s += "" + S[i] + ", "
+            else:
+                s += "_, "
+        if S[12] != '_':
+            s += "" + S[i] + ")"
+        else:
+            s += "_)"
         return s
 
     def start(self):
@@ -176,18 +188,21 @@ prolog = Prolog()
 dfD = pd.read_csv('Destinations.csv')
 prolog.retractall("destination(_, _, _, _, _, _, _, _, _, _, _, _, _)")
 for index, row in dfD.iterrows():
-    s = 'destination("'
-    s += row['Destinations'] + '","' +  row['country'] + '","' + row['region'] + '","' + row['Climate'] + '","' + row['Budget'] + '","'
-    s += row['Activity'] + '","' + row['Demographics'] + '","' + row['Duration'] + '","' + row['Cuisine'] + '","'
-    s += row['History'] + '","' + row['Natural Wonder'] + '","' + row['Accommodation'] + '","' + row['Language'] + '")'
+    s = "destination('"
+    s += row['Destinations'].replace("'", "") + "', '" +  row['country'] + "', '" + row['region'] + "', '" + row['Climate'] + "', '" + row['Budget'] + "', '"
+    s += row['Activity'] + "', '" + row['Demographics'] + "', '" + row['Duration'] + "', '" + row['Cuisine'] + "', '"
+    s += row['History'] + "', '" + row['Natural Wonder'] + "', '" + row['Accommodation'] + "', '" + row['Language'] + "')"
     prolog.assertz(s)
 # prolog.assertz("destination('Tokyo', japan, 'East Asia', temperate, high, cultural, solo, long, asian, modern, mountains, luxury, japanese)")
-# prolog.assertz("destination('Ottawa', canada, 'North America', cold, medium, adventure, family_friendly, medium, european, modern, forests, mid_range, english)")
+# prolog.assertz("destination('Ottawa', 'canada', 'North America', 'cold', 'medium', 'adventure', 'family_friendly', 'medium', 'european', 'modern', 'forests', 'mid_range', 'english')")
 # prolog.assertz("destination('Mexico City', mexico, 'North America', temperate, low, cultural, senior, short, latin_american, ancient, mountains, budget, spanish)")
-# prolog.assertz("destination('Rome', italy, 'Southern Europe', temperate, high, cultural, solo, medium, european, ancient, beaches, luxury, italian)")
-# prolog.assertz("destination('Brasilia', brazil, 'South America', tropical, low, adventure, family_friendly, long, latin_american, modern, beaches, budget, portuguese)")
+# prolog.assertz('destination("Rome", italy, "Southern Europe", temperate, high, cultural, solo, medium, european, ancient, beaches, luxury, italian)')
+# prolog.assertz('destination("Brasilia", "brazil", "South America", "tropical", "low", "adventure", "family_friendly", "long", "latin_american", "modern", "beaches", "budget", "portuguese")')
 
-
+# query = "destination(City, 'canada', 'North America', 'cold', 'medium', 'adventure', 'family_friendly', 'medium', 'european', 'modern', 'forests', 'mid_range', 'english')"
+# results = list(prolog.query(query))
+# for result in results:
+#     print(result['City'])
 
 # TODO 2: extract unique features from the Destinations.csv and save them in a dictionary
 ################################################################################################
